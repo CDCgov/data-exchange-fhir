15d82c4fdd6dc219ad1196dd46d74496eac71229
Cosmin Potocean <cosmycx@gmail.com>
2023-05-16 14:30:49 -0400

    spikes lrp4 node app in prog

--- spikes/smart-node/app.js
+++ spikes/smart-node/app.js
@@ -1,10 +1,10 @@
-
-
 ( async function start() {
 
     const dotenv = require('dotenv').config()
+    const fhirclient = require('fhirclient')
 
     console.log('starting smart auth node app..')
+    console.log('loading environment variables..')
 
     const serverUrl = process.env.SERVER_URL
     const clientId = process.env.CLIENT_ID
@@ -14,6 +14,29 @@
     console.log(`clientId: ${clientId}`)
     console.log(`redirectUri: ${redirectUri}`) 
 
+
+    // Create an instance of the SMART client
+    const client = fhirclient({
+        serviceUrl: serverUrl,
+        clientId: clientId,
+        redirectUri: redirectUri,
+        scope: "launch openid fhirUser patient/*.read",
+    })
+
+    // Launch the SMART client authorization flow
+    client.authorize({
+        clientId: client.clientId,
+        scope: client.scope,
+        redirectUri: client.redirectUri,
+        // launch: launchUri,
+      }).then(tokenResponse => {
+        // Access token obtained successfully
+        console.log('Access token:', tokenResponse.access_token)
+
+      }).catch(error => {
+        // Handle errors
+        console.error('An error occurred:', error)
+      })
 
 
 })()
