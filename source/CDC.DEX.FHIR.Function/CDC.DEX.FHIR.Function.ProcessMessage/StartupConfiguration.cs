68200493059b86b729edbc22ba943c09ee0faeca
csalmi56 <ac56@cdc.gov>
2024-10-01 14:18:48 -0400

    Wrapped config start in try/catch to eliminate errors incase config was not able to connect to keyvault

--- source/CDC.DEX.FHIR.Function/CDC.DEX.FHIR.Function.ProcessMessage/StartupConfiguration.cs
+++ source/CDC.DEX.FHIR.Function/CDC.DEX.FHIR.Function.ProcessMessage/StartupConfiguration.cs
@@ -18,14 +18,21 @@ namespace CDC.DEX.FHIR.Function.ProcessMessage.Config
         public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
         {
             string cs = Environment.GetEnvironmentVariable("FhirFunctionAppConfigConnectionString");
-            builder.ConfigurationBuilder.AddAzureAppConfiguration(options =>
+            try
             {
-                options.Connect(cs)
-                       .ConfigureKeyVault(kv =>
-                       {
-                           kv.SetCredential(new DefaultAzureCredential());
-                       });
-            });
+                builder.ConfigurationBuilder.AddAzureAppConfiguration(options =>
+                {
+                    options.Connect(cs)
+                           .ConfigureKeyVault(kv =>
+                           {
+                               kv.SetCredential(new DefaultAzureCredential());
+                           });
+                });
+            }
+            catch (Exception e)
+            {
+                log.LogInformation("ProcessMessage not connecting to App Configuration or KeyVault.");
+            }
         }
 
         public override void Configure(IFunctionsHostBuilder builder)
