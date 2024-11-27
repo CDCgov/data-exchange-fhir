ddd46447f7250aff61baa9e809af74b6a1e96f8d
csalmi56 <ac56@cdc.gov>
2024-11-27 12:22:35 -0500

    Updated program.cs with new configs

--- spikes/fhir-facade/Program.cs
+++ spikes/fhir-facade/Program.cs
@@ -22,7 +22,7 @@ namespace OneCDPFHIRFacade
             // UseLocalDevFolder to true for Local development and Not AWS
             // UseLocalDevFolder to false will be using AWS
             // #####################################################
-            var useLocalDevFolder = builder.Configuration.GetValue<bool>("UseLocalDevFolder");
+            var useLocalDevFolder = LocalFileStorageConfig.UseLocalDevFolder;
             var useAWSS3 = !useLocalDevFolder;
 
             // Register serivces, Create instances of LocalFileService and S3FileService
@@ -59,70 +59,6 @@ namespace OneCDPFHIRFacade
             }
 
             app.UseHttpsRedirection();
-
-            // #####################################################
-            // POST endpoint for Bundle
-            // #####################################################
-            //app.MapPost("/Bundle", async (HttpContext httpContext, ILocalFileService localFileService, IS3FileService s3FileService) =>
-            //{
-
-            //    var parser = new FhirJsonParser();
-            //    Bundle bundle;
-
-            //    try
-            //    {
-            //        // Read the request body as a string
-            //        var requestBody = await new StreamReader(httpContext.Request.Body).ReadToEndAsync();
-            //        // Parse JSON string to FHIR Bundle object
-            //        bundle = parser.Parse<Bundle>(requestBody);
-
-            //        // Check if Patient ID is present
-            //        if (string.IsNullOrWhiteSpace(bundle.Id))
-            //        {
-            //            return Results.BadRequest(new
-            //            {
-            //                error = "Invalid payload",
-            //                message = "Resource ID is required."
-            //            });
-            //        }
-
-            //        // Log details to console
-            //        Console.WriteLine($"Received FHIR Bundle: Id={bundle.Id}");
-
-            //        // Generate a new UUID for the file name
-            //        var fileName = $"{Guid.NewGuid()}.json";
-            //        var resourceJson = bundle.ToJson();
-
-            //        if (useLocalDevFolder)
-            //        {
-            //            // #####################################################
-            //            // Save the FHIR Resource Locally
-            //            // #####################################################
-            //            return await localFileService.SaveResourceLocally(localReceivedFolder, "Bundle", fileName, resourceJson);
-
-            //        } // .if UseLocalDevFolder
-            //        else
-            //        {
-            //            // #####################################################
-            //            // Save the FHIR Resource to AWS S3
-            //            // #####################################################
-            //            if (AwsConfig.S3Client == null || string.IsNullOrEmpty(AwsConfig.BucketName))
-            //            {
-            //                return Results.Problem("S3 client and bucket are not configured.");
-            //            }
-            //            return await s3FileService.SaveResourceToS3(AwsConfig.S3Client, AwsConfig.BucketName, "Bundle", fileName, resourceJson);
-            //        }// .else
-            //    }
-            //    catch (FormatException ex)
-            //    {
-            //        // Return 400 Bad Request if JSON is invalid
-            //        return Results.BadRequest(new
-            //        {
-            //            error = "Invalid payload",
-            //            message = $"Failed to parse FHIR Resource: {ex.Message}"
-            //        });
-            //    }
-            //});
 
             app.MapControllers();
             // #####################################################
