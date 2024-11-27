68fd5792c0ca5e91df207ba41f40565b2ea46f05
csalmi56 <ac56@cdc.gov>
2024-11-27 12:14:13 -0500

    Moved Bundle to its own controller

---
+++ spikes/fhir-facade/Config/LocalFileStorageConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace OneCDPFHIRFacade.Config
+{
+    public static class LocalFileStorageConfig
+    {
+        public static string KeyName = "FileSettings";
+        public static bool UseLocalDevFolder { get; set; }
+        public static string? LocalDevFolder { get; set; }
+
+        public static void Initialize(IConfiguration configuration)
+        {
+            var section = configuration.GetSection(KeyName);
+            UseLocalDevFolder = section.GetValue<bool>("UseLocalDevFolder");
+            LocalDevFolder = section.GetValue<string>("LocalDevFolder");
+        }
+    }
+}
---
+++ spikes/fhir-facade/Controllers/BundleController.cs
@@ -0,0 +1,79 @@
+﻿using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using OneCDPFHIRFacade.Config;
+using OneCDPFHIRFacade.Services;
+
+namespace OneCDPFHIRFacade.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BundleController : ControllerBase
+    {
+        [HttpPost(Name = "PostBundle")]
+        public async Task<IResult> Post()
+        {
+            LocalFileService localFileService = new LocalFileService();
+            S3FileService s3FileService = new S3FileService();
+
+            // Use FhirJsonParser to parse incoming JSON as FHIR bundle
+            var parser = new FhirJsonParser();
+            Bundle bundle;
+
+            try
+            {
+                // Read the request body as a string
+                var requestBody = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+                // Parse JSON string to FHIR bundle object
+                bundle = parser.Parse<Bundle>(requestBody.ToString());
+            }
+            catch (FormatException ex)
+            {
+                // Return 400 Bad Request if JSON is invalid
+                return Results.BadRequest(new
+                {
+                    error = "Invalid payload",
+                    message = $"Failed to parse FHIR Resource: {ex.Message}"
+                });
+            }
+
+            // Check if bundle ID is present
+            if (string.IsNullOrWhiteSpace(bundle.Id))
+            {
+                return Results.BadRequest(new
+                {
+                    error = "Invalid payload",
+                    message = "Resource ID is required."
+                });
+            }
+
+            // Log details to console
+            Console.WriteLine($"Received FHIR Bundle: Id={bundle.Id}");
+
+            // Generate a new UUID for the file name
+            var fileName = $"{Guid.NewGuid()}.json";
+
+            if (LocalFileStorageConfig.UseLocalDevFolder)
+            {
+                // #####################################################
+                // Save the FHIR Resource Locally
+                // #####################################################
+                return await localFileService.SaveResourceLocally(LocalFileStorageConfig.LocalDevFolder!, "Bundle", fileName, bundle.ToJson());
+
+            } // .if UseLocalDevFolder
+            else
+            {
+                // #####################################################
+                // Save the FHIR Resource to AWS S3
+                // #####################################################
+                if (AwsConfig.S3Client == null || string.IsNullOrEmpty(AwsConfig.BucketName))
+                {
+                    return Results.Problem("S3 client and bucket are not configured.");
+                }
+
+                return await s3FileService.SaveResourceToS3(AwsConfig.S3Client, AwsConfig.BucketName, "Bundle", fileName, bundle.ToJson());
+            }// .else
+        }
+
+    }
+}
