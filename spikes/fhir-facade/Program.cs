0153a20fe4b95aa5c4744a4aa35285d309ff19f4
csalmi56 <ac56@cdc.gov>
2024-12-12 11:06:17 -0500

    Added cloud watch telemetry

--- spikes/fhir-facade/Program.cs
+++ spikes/fhir-facade/Program.cs
@@ -3,6 +3,10 @@ using Amazon.Runtime;
 using Amazon.S3;
 using OneCDPFHIRFacade.Config;
 using OneCDPFHIRFacade.Services;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace OneCDPFHIRFacade
 {
@@ -11,6 +15,7 @@ namespace OneCDPFHIRFacade
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var resourceBuilder = ResourceBuilder.CreateDefault().AddService("OneCDPFHIRFacade");
 
             // Add services to the container.
             builder.Services.AddEndpointsApiExplorer();
@@ -48,6 +53,44 @@ namespace OneCDPFHIRFacade
                 // Initialize the client with credentials and config
                 AwsConfig.S3Client = new AmazonS3Client(new BasicAWSCredentials(AwsConfig.AccessKey, AwsConfig.SecretKey), s3Config);
             }// .if
+
+            //CloudWatchLoggerService logEntry = new CloudWatchLoggerService();
+
+            if (!string.IsNullOrEmpty(AwsConfig.OltpEndpoint))
+            {
+                //logEntry.AppendLogAsync(AwsConfig.OltpEndpoint, " ProgramOLTP ");
+                builder.Services.AddOpenTelemetry().WithTracing(tracerProviderBuilder =>
+               {
+                   tracerProviderBuilder
+                       .SetResourceBuilder(resourceBuilder)
+                       .AddAspNetCoreInstrumentation() // Instruments ASP.NET Core (HTTP request handling)
+                       .AddHttpClientInstrumentation() // Instruments outgoing HTTP client requests
+                       .AddAWSInstrumentation()
+                       .AddConsoleExporter()
+                       .AddOtlpExporter(options =>
+                       {
+                           options.Endpoint = new Uri(AwsConfig.OltpEndpoint); // "http://localhost:4317");
+                           options.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+                       });
+               });
+
+
+                builder.Services.AddOpenTelemetry().WithMetrics(metricsProviderBuilder =>
+                {
+                    metricsProviderBuilder
+                        .SetResourceBuilder(resourceBuilder)
+                        .AddAspNetCoreInstrumentation() // Collect ASP.NET Core metrics
+                        .AddHttpClientInstrumentation() // Collect HTTP client metrics
+                        .AddMeter("OneCDPFHIRFacadeMeter").AddOtlpExporter(options =>
+                        {
+                            options.Endpoint = new Uri(AwsConfig.OltpEndpoint); // "http://localhost:4317");                   
+                        }).AddConsoleExporter();  // Custom metrics              
+                });
+            }
+            else
+            {
+                //logEntry.AppendLogAsync("No OLTP", " ProgramOLTP ");
+            }
 
             var app = builder.Build();
 
