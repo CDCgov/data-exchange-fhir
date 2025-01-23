3a32db28fad4eb847a267603225fe9fe4eb366cb
csalmi56 <ac56@cdc.gov>
2025-01-23 17:36:13 -0500

    Updated OpenTelemetry

--- source/fhir-facade/src/Services/S3FileService.cs
+++ source/fhir-facade/src/Services/S3FileService.cs
@@ -22,6 +22,31 @@ namespace OneCDPFHIRFacade.Services
         {
             this.LoggingUtility = loggingUtility;
         }
+
+        public async Task<IResult> SaveOpenTelemetryToS3(string keyPrefix, string fileName, string resourceJson, LoggerService logEntry, string requestId)
+        {
+            // Define the S3 put request
+            var putRequest = new PutObjectRequest
+            {
+                BucketName = AwsConfig.BucketName,
+                Key = $"{keyPrefix}/{fileName}",
+                ContentBody = resourceJson
+            };
+
+            try
+            {
+                var response = await AwsConfig.S3Client!.PutObjectAsync(putRequest);
+                LoggingUtility.Logging(logEntry.ToString(), requestId);
+                return Results.Ok($"Telemtry Saved to S3 bucket {fileName}");
+            }
+            catch (Exception ex)
+            {
+                string logMessage = $"Error saving resource to S3: {ex.Message}";
+                LoggingUtility.Logging(logMessage, requestId);
+                LoggingUtility.SaveLogS3(requestId);
+                return Results.Problem($"Error saving resource to S3: {ex.Message}");
+            }
+        }
         public async Task<IResult> SaveResourceToS3(string keyPrefix, string fileName, string resourceJson, LoggerService logEntry, string requestId)
         {
             // Define the S3 put request
