81f9f148b0691cb9d304a7cf569cac789f9b1cc8
csalmi56 <ac56@cdc.gov>
2025-03-18 12:53:53 -0400

    Added files

--- spikes/SyntheaCreateLargeFiles/SyntheaCreateLargeFiles/LargeFileFromSynthea.cs
+++ spikes/SyntheaCreateLargeFiles/SyntheaCreateLargeFiles/LargeFileFromSynthea.cs
@@ -23,18 +23,18 @@ namespace SyntheaCreateLargeFiles
                         {
                             subFolder = "5mb";
                         }
-                        else if (fileSizeInMb > 9 && fileSizeInMb < 11)
-                        {
-                            subFolder = "10mb";
-                        }
-                        else if (fileSizeInMb > 24 && fileSizeInMb < 26)
-                        {
-                            subFolder = "25mb";
-                        }
-                        else if (fileSizeInMb > 49 && fileSizeInMb < 51)
-                        {
-                            subFolder = "50mb";
-                        }
+                        //else if (fileSizeInMb > 9 && fileSizeInMb < 11)
+                        //{
+                        //    subFolder = "10mb";
+                        //}
+                        //else if (fileSizeInMb > 24 && fileSizeInMb < 26)
+                        //{
+                        //    subFolder = "25mb";
+                        //}
+                        //else if (fileSizeInMb > 49 && fileSizeInMb < 51)
+                        //{
+                        //    subFolder = "50mb";
+                        //}
 
                         if (subFolder != null)
                         {
@@ -55,9 +55,19 @@ namespace SyntheaCreateLargeFiles
 
                             File.WriteAllText(outputFile, jsonObject.ToString());
 
-                            Console.WriteLine($"Copied and modified {inputFileName} to {outputFile}");
-                        }
-                    }
+                            // Read and modify JSON
+                            string jsonContent = File.ReadAllText(file);
+                            JObject jsonObject = JObject.Parse(jsonContent);
+
+                            if (jsonObject["resourceType"]?.ToString() == "Bundle")
+                            {
+                                jsonObject["id"] = "123"; // Add id field
+                                jsonObject["meta"] = new JObject
+                                {
+                                    ["profile"] = new JArray("http://hl7.org/fhir/us/ecr/StructureDefinition/eicr-document-bundle")
+                                };
+                            }
+
                 }
                 else
                 {
