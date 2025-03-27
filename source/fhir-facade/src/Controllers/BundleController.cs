4de8d1aa5f346443cb134adda38a645c02bf2390
csalmi56 <ac56@cdc.gov>
2025-03-27 10:43:05 -0400

    Changed back to stream reader

--- source/fhir-facade/src/Controllers/BundleController.cs
+++ source/fhir-facade/src/Controllers/BundleController.cs
@@ -80,10 +80,9 @@ namespace OneCDPFHIRFacade.Controllers
                             { "message", "No file uploaded or file is empty." }
                         });
                     }
-                    using var memoryStream = new MemoryStream();
-                    await file.CopyToAsync(memoryStream);
-                    memoryStream.Seek(0, SeekOrigin.Begin); // Reset position
-                    fileContent = await new StreamReader(memoryStream).ReadToEndAsync();
+                    using var reader = new StreamReader(file.OpenReadStream());
+                    fileContent = await reader.ReadToEndAsync();
+
                 }
                 //Read from body
                 else if (HttpContext.Request.ContentType != null &&
